66be87cb2f625bc2cabbe9d47625005edfcd9645
Alexandre Samxaradze <alexandresamxaradze@Alexandres-Mac-mini.local>
2022-06-16 23:04:16 +0400

    added imgs

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+import img1 from  "./images/1.webp"
+import img2 from  "./images/2.webp"
+import img3 from  "./images/3.webp"
+import img4 from  "./images/4.webp"
+import img5 from  "./images/5.webp"
+import img6 from  "./images/6.webp"
+import img7 from  "./images/7.webp"
+import img8 from  "./images/8.webp"
+import img9 from  "./images/9.webp"
+import img10 from  "./images/10.webp"
+import img11 from  "./images/11.webp"
+import img12 from  "./images/12.webp"
+import img13 from  "./images/13.webp"
+import img14 from  "./images/14.webp"
+import img15 from  "./images/15.webp"
+import img16 from  "./images/16.webp"
+import img17 from  "./images/17.webp"
+import img18 from  "./images/18.webp"
+import img19 from  "./images/19.webp"
+import img20 from  "./images/20.webp"
+import img21 from  "./images/21.webp"
+import img22 from  "./images/22.webp"
+import img23 from  "./images/23.webp"
+import img24 from  "./images/24.webp"
+import img25 from  "./images/25.webp"
+import img26 from  "./images/26.webp"
+
 const body = document.body;
 const main = document.getElementById("main");
 
@@ -159,32 +186,32 @@ setTimeout(() => {
 
 
 const imageAddresses = [
-  "images/1.webp",
-  "images/2.webp",
-  "images/3.webp",
-  "images/4.webp",
-  "images/5.webp",
-  "images/6.webp",
-  "images/7.webp",
-  "images/8.webp",
-  "images/9.webp",
-  "images/10.webp",
-  "images/11.webp",
-  "images/12.webp",
-  "images/13.webp",
-  "images/14.webp",
-  "images/15.webp",
-  "images/16.webp",
-  "images/17.webp",
-  "images/18.webp",
-  "images/19.webp",
-  "images/20.webp",
-  "images/21.webp",
-  "images/22.webp",
-  "images/23.webp",
-  "images/24.webp",
-  "images/25.webp",
-  "images/26.webp"
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26
 ];
 
 const init = () => {
